8089a5c2144eac4653fe670e4b2453fa4c20b650
Nick <nickmarron@hotmail.co.uk>
2020-05-12 21:40:27 +0100

    name done

--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,7 @@
-function Pet() {}
-
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+    this.growOlder()
+}
+Pet.prototype = { growUp}
 module.exports = Pet;
--- tests/pet.test.js
+++ tests/pet.test.js
@@ -1,7 +1,24 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns an object', () => {
+    xit('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.name).toEqual('Fido');
+      });
+
+      it('sets the age property', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.age).toEqual(0);
+      });
+      it('increases the age of the pet by 1', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.growOlder).toEqual(0);
+      });
   });
