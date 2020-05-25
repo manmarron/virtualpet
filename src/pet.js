2434e2c08ad769e9c553166e31eb6f3a32072743
Nick <nickmarron@hotmail.co.uk>
2020-05-25 20:58:53 +0100

    finished virtual pet

--- src/pet.js
+++ src/pet.js
@@ -1,7 +1,64 @@
+const MIN_HUNGER = 0;
+const MAXIMUM_FITNESS = 10;
+const FEEDMSG = 'I am hungry'
+const FITNESSMSG = 'I need a walk'
+const HUNGERCHECKLEVEL = 5
+const FITNESSCHECKLEVEL = 3
+const OKMSG = 'I feel great!'
+
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
-    this.growOlder()
-}
-Pet.prototype = { growUp}
-module.exports = Pet;
+    this.hunger = MIN_HUNGER;
+    this.fitness = MAXIMUM_FITNESS;
+    this.checkUpMsg = ''
+    
+    }
+
+    Pet.prototype = {
+        get isAlive() {
+          return this.age < 30 && this.hunger < 15 && this.fitness > 0;
+        }
+      };  
+
+    Pet.prototype.growUp = function() {
+        if (!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+          }
+        this.age += 1;
+        this.hunger += 5;
+        this.fitness -= 3;
+    }
+
+    Pet.prototype.walk = function() {
+        if (!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+          }
+        this.fitness += 4;
+        if (this.fitness >= MAXIMUM_FITNESS) {this.fitness = MAXIMUM_FITNESS};
+    };
+
+    Pet.prototype.feed = function() {
+        if (!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+          }
+        this.hunger -= 3;
+        if (this.hunger <= MIN_HUNGER) {this.hunger = MIN_HUNGER};
+
+    };
+
+    Pet.prototype.checkUp = function() {
+        if (this.fitness <= FITNESSCHECKLEVEL && this.hunger >= HUNGERCHECKLEVEL){
+            this.checkUpMsg = FEEDMSG + ' and ' + FITNESSMSG
+        } else if (this.fitness <= FITNESSCHECKLEVEL) {
+            this.checkUpMsg = FITNESSMSG
+        }else if (this.hunger >= HUNGERCHECKLEVEL) {
+            this.checkUpMsg = FEEDMSG
+        }else if (this.fitness > FITNESSCHECKLEVEL && this.hunger < HUNGERCHECKLEVEL){
+            this.checkUpMsg = OKMSG
+        }; 
+
+
+    };
+module.exports = Pet
--- tests/pet.test.js
+++ tests/pet.test.js
@@ -1,24 +1,119 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    xit('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+
+    it('returns an object', () => {
+      const fido = new Pet('Maverick Meerkat');
+      expect(new Pet('Maverick Meerkat')).toBeInstanceOf(Object);
     });
 
-    it('sets the name property', () => {
-        const pet = new Pet('Fido');
-    
-        expect(pet.name).toEqual('Fido');
-      });
+    it('returns pet name as fido', () => {
+      const pet = new Pet('Maverick Meerkat');
+      expect(pet.name).toEqual('Maverick Meerkat');
+    });
 
-      it('sets the age property', () => {
-        const pet = new Pet('Fido');
-    
-        expect(pet.age).toEqual(0);
-      });
-      it('increases the age of the pet by 1', () => {
-        const pet = new Pet('Fido');
-    
-        expect(pet.growOlder).toEqual(0);
-      });
+    it('returns pet age', () => {
+      const pet = new Pet('Maverick Meerkat');
+      expect(pet.age).toEqual(0);
+    });
+
+    it('returns pet age once grown up', () => {
+      const pet = new Pet('Maverick Meerkat');
+      pet.growUp()
+      expect(pet.age).toEqual(1);
+      pet.growUp()
+      expect(pet.age).toEqual(2);
+      pet.growUp()
+      expect(pet.age).toEqual(3);
+
+    });
+
+    it('hunger+5, age +1, fitness -3', () => {
+      const pet = new Pet('Maverick Meerkat');
+      pet.growUp()
+      expect(pet.age).toEqual(1);
+      expect(pet.hunger).toEqual(5);
+      expect(pet.fitness).toEqual(7);
+      pet.growUp()
+      expect(pet.age).toEqual(2);
+      expect(pet.hunger).toEqual(10);
+      expect(pet.fitness).toEqual(4);
+      pet.growUp()
+      expect(pet.age).toEqual(3);
+      expect(pet.hunger).toEqual(15);
+      expect(pet.fitness).toEqual(1);
+    });
+
+    it('exercise pet - increase fitness by 4', () => {
+      const pet = new Pet('Maverick Meerkat');
+      pet.growUp()
+      pet.walk();
+      expect(pet.fitness).toEqual(10);
+    });
+
+    it('Feed pet - decrease hunger by 3', () => {
+      const pet = new Pet('Maverick Meerkat');
+      pet.growUp()
+      pet.feed();
+      expect(pet.hunger).toEqual(2);
+      pet.feed();
+      expect(pet.hunger).toEqual(0);
+      pet.feed();
+      expect(pet.hunger).toEqual(0);
+    });
+
+
+    xit('check up on the pet levels', () => {
+      const pet = new Pet('Maverick Meerkat');
+      pet.checkUp()
+      expect(pet.checkUpMsg).toEqual('I feel great!');
+
+
+      pet.growUp()      
+      pet.growUp()      
+      pet.growUp()
+      pet.feed()
+      pet.feed();
+      pet.checkUp()
+      expect(pet.checkUpMsg).toEqual('I am hungry and I need a walk');
+
+      pet.feed();
+      pet.feed();
+      pet.feed();
+      pet.feed();
+      pet.feed();
+      pet.checkUp();
+      expect(pet.checkUpMsg).toEqual('I need a walk');
+
+
+      pet.walk();
+      pet.walk();
+      pet.checkUp();
+      expect(pet.checkUpMsg).toEqual('I feel great!');
+
+    });
+
+    it('returns if pet is alive', () => {
+      const pet = new Pet('Maverick Meerkat');
+
+      pet.walk();
+      expect(pet.isAlive).toEqual(true);
+      pet.growUp();
+      pet.growUp();
+      pet.growUp();
+      expect(pet.isAlive).toEqual(false);
+    });
+
+
+    it('kills pet- stops any other processes running', () => {
+      const pet = new Pet('Maverick Meerkat')
+      pet.age = 30;
+      pet.hunger = 16;
+      pet.fitness = 1;
+      expect(() => pet.feed()).toThrow('Your pet is no longer alive :(');
+      expect(() => pet.walk()).toThrow('Your pet is no longer alive :(');
+      expect(() => pet.growUp()).toThrow('Your pet is no longer alive :(');
+    });
   });
+
+
